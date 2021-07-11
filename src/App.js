cb469c13b83bbe52f0e3078b22f47be342b50ba3
anisur2805 <anisur2805@gmail.com>
2021-07-12 00:57:03 +0600

    provate route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Main from './components/Main';
+import ProvideAuth from './components/ProvideAuth';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello From React Router DOM</h1>
-      <Main />
+      {/* <Main /> */}
+      <ProvideAuth />
     </div>
   );
 }
