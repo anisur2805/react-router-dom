98f4c63b5a4b8b1ee31cb53450e8760d3adbfc60
anisur2805 <anisur2805@gmail.com>
2021-07-12 01:51:31 +0600

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+/* eslint-disable no-unused-vars */
 import './App.css';
 import Main from './components/Main';
 import ProvideAuth from './components/ProvideAuth';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello From React Router DOM</h1>
       {/* <Main /> */}
-      <ProvideAuth />
+      {/* <ProvideAuth /> */}
+      <Sidebar />
     </div>
   );
 }
